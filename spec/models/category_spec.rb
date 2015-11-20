a0355e5040423a7163d0fe1c7099f9d7c94670b1
Dean Silfen <dean.silfen@gmail.com>
2015-11-20 09:21:19 -0500

    sketch category specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) { FactoryGirl.create(:category)
+  
+  describe ".enable!" do
+    it "activates a category"
+    it "does nothing to an active category"
+  end
+
+  describe ".disable!" do
+    it "deactivates a category"
+    it "does nothing to a deactivated category"
+  end
+
 end
