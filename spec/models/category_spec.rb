f8c2336676927a10268ce14b95a68e4d47f64710
Dean Silfen <dean.silfen@gmail.com>
2015-11-21 01:56:31 -0500

    Try to fix specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,15 +3,23 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
   let(:category) { FactoryGirl.create(:category)
-  
+
   describe ".enable!" do
-    it "activates a category"
-    it "does nothing to an active category"
+    it "activates a category" do
+      category.update(active: false)
+      expect(category.enable!).to be_truthy
+    end
+
+    it "does nothing to an active category" do
+      category.update(active: true)
+      expect(category.enable!).to be_falsey
+    end
+
   end
 
   describe ".disable!" do
     it "deactivates a category"
     it "does nothing to a deactivated category"
   end
-
 end
+
